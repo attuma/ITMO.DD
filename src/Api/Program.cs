1602e6c64ef70b143af43a6b29cda55873232ce5
attuma <klogdanidi@list.ru>
2026-06-14 22:00:42 +0300

    Add Api project with Swagger, docker-compose for Postgres, and initial EF Core migration

---
+++ src/Api/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using StudentTracker.Infrastructure.Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.Run();
